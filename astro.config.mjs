e3d0f563ca5025044614a3b3a62c5576b1f90643
asepngrh <asepnugraha105@gmail.com>
2026-04-15 17:41:52 +0700

    fix: ubah astro ke static mode untuk cloudflare pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,8 @@ import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  adapter: cloudflare(),
+  // output: 'server', // Dihapus karena frontend ini hanya static HTML + JS
+  // adapter: cloudflare(), 
   devToolbar: {
     enabled: false
   }
